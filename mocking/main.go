b6d19f46c468fcdf45398a727fd8f18977de188e
aunn <aunn_it08@hotmail.com>
2018-07-20 21:41:08 +0700

    wip mocking

---
+++ mocking/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type RealSleeper struct{}
+
+func (s *RealSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	Countdown(os.Stdout, &RealSleeper{})
+}
+
+const finalWord = "Go!"
+const coundownStart = 3
+
+func Countdown(w io.Writer, s Sleeper) {
+	for i := coundownStart; i > 0; i-- {
+		s.Sleep()
+		fmt.Fprintf(w, "%d\n", i)
+	}
+
+	s.Sleep()
+	fmt.Fprintf(w, finalWord)
+}
