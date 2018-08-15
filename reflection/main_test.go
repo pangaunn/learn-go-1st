c9ae98ca850c974172b38b99c0d4a0d5d4e32953
aunn <aunn_it08@hotmail.com>
2018-08-15 20:07:56 +0700

    make test pass

--- reflection/main_test.go
+++ reflection/main_test.go
@@ -1,3 +1,5 @@
+package main
+
 import "testing"
 
 func TestWalk(t *testing.T) {
@@ -8,11 +10,11 @@ func TestWalk(t *testing.T) {
 		Name string
 	}{expected}
 
-	walk(x, func(input string) {
+	Walk(x, func(input string) {
 		got = append(got, input)
 	})
 
-	if len(got) != 1 {
+	if got[0] != expected {
 		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
 	}
 }
