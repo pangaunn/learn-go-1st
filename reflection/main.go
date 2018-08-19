16d2f59f2c9b4c362b27ba521bae055bbba7582e
aunn <aunn_it08@hotmail.com>
2018-08-19 21:52:25 +0700

    finish reflection

--- reflection/main.go
+++ reflection/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "reflect"
+
 func Walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	val := getValue(x)
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
+	}
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
 }
