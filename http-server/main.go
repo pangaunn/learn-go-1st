86b4d5ab619bf01ab92ada0c52d7d7eeecc869a7
pangaunn <aunn_it08@hotmail.com>
2018-09-25 13:06:41 +0700

    add inmemory store

--- http-server/main.go
+++ http-server/main.go
@@ -18,8 +18,14 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
